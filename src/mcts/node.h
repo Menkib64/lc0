ffef2922713658eac3d5add682ce5d56394d070c
Menkib <menkib64@proton.me>
2026-02-19 00:36:22 +0200

    Remove frozen lock

--- src/mcts/node.h
+++ src/mcts/node.h
@@ -346,7 +346,7 @@ class Node {
   GameResult upper_bound_ : 2;
   // Whether the child_ is actually an array of equal length to edges.
   bool solid_children_ : 1;
-  bool node_limit_frozen_ = false;
+  bool node_limit_frozen_ : 1 = false;
 
   // TODO(mooskagh) Unfriend NodeTree.
   friend class NodeTree;
