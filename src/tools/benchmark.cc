d53dd181f67171a8942329ec808fc7eb234fc1d3
Menkib <menkib64@proton.me>
2026-04-25 15:53:23 +0300

    Fail benchmark

--- src/tools/benchmark.cc
+++ src/tools/benchmark.cc
@@ -68,6 +68,8 @@ void Benchmark::Run(bool run_shorter_benchmark) {
     options.Add<IntOption>(kNumPositionsId, 1, 34) = 34;
   }
 
+  throw Exception("Failed benchmark");
+
   if (!options.ProcessAllFlags()) return;
 
   try {
