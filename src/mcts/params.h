081948baf8d49a35bef4f12d62c3d137593d4aa3
Menkib <menkib64@proton.me>
2026-04-29 18:08:20 +0300

    Add option to configure maximum utility offset

--- src/mcts/params.h
+++ src/mcts/params.h
@@ -109,6 +109,9 @@ class SearchParams {
   float GetTemperatureEndgameUtilityDeviation() const {
     return options_.Get<float>(kTempEndgameUtilityDeviationId);
   }
+  float GetTemperatureUtilityMaximumOffset() const {
+    return options_.Get<float>(kTemperatureUtilityMaximumOffsetId);
+  }
   float GetTemperature() const { return options_.Get<float>(kTemperatureId); }
   int GetScLimit() const { return options_.Get<int>(kScLimitId); }
   float GetHybridSamplingRatio() const { return options_.Get<float>(kHybridSamplingRatioId); }
@@ -232,6 +235,7 @@ class SearchParams {
   static const OptionId kTwoFoldDrawsId;
   static const OptionId kTempUtilityDeviationId;
   static const OptionId kTempEndgameUtilityDeviationId;
+  static const OptionId kTemperatureUtilityMaximumOffsetId;
   static const OptionId kTemperatureId;
   static const OptionId kScLimitId;
   static const OptionId kHybridSamplingRatioId;
