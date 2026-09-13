5891d90ae32d1809284416a9df8ca054138aa545
Menkib <menkib64@proton.me>
2026-09-13 10:15:07 +0300

    Fix hip compilation warnings.

--- src/neural/backends/cuda/fp16_kernels.cu
+++ src/neural/backends/cuda/fp16_kernels.cu
@@ -454,13 +454,13 @@ void OutputInputTransform(int N, int C, int se_K, T* output, const T* input,
     // Use special kernel with reduced register pressure - only works on Ampere,
     // and only for fp16.
     if (C <= kMaxResBlockFusingSeKFp16Ampere) {
-      cudaFuncSetAttribute(
+      ReportCUDAErrors(cudaFuncSetAttribute(
 #if defined(USE_HIP)
           (const void*)
 #endif
           OutputInputTransformKernel_fp16_shmem_board<activation, use_bias,
                                                       use_skip>,
-          cudaFuncAttributeMaxDynamicSharedMemorySize, 72 * C * sizeof(half));
+          cudaFuncAttributeMaxDynamicSharedMemorySize, 72 * C * sizeof(half)));
       OutputInputTransformKernel_fp16_shmem_board<activation, use_bias,
                                                   use_skip>
           <<<N, C, 72 * C * sizeof(half), stream>>>(
